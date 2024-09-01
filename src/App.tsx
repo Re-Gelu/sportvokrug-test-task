8c100ca76ddda3d4adf18e8ec9ca6f1ee0c9456d
Re:Gelu <morandva@yandex.ru>
2024-09-01 19:59:30 +0300

    App dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,8 @@ import configuration from "@/shared/configuration";
 import { ActiveEventScreen, Clock, CountdownEventScreen } from "@/widgets";
 import { gql, useQuery } from "@apollo/client";
 
-const testData = {
-  videostandEvents: {
-    current_and_upcoming: [
-      {
-        __typename: "VideostandEvent", // 2
-        title: "Чемпионат Москвы",
-        is_main: false,
-        dt_start: "2024-09-02T10:00:00+03:00",
-        dt_end: "2024-09-03T23:59:00+03:00",
-        dt_create: "2024-01-31T02:39:01+03:00",
-      },
-      /* {
-        __typename: "VideostandEvent", // 1
-        title: "Первенство Москвы",
-        is_main: true,
-        dt_start: "2024-08-30T10:00:00+03:00",
-        dt_end: "2024-09-04T21:00:00+03:00",
-        dt_create: "2024-01-24T05:56:29+03:00",
-      }, */
-      {
-        __typename: "VideostandEvent", // 3
-        title:
-          'Международные соревнования "Гран-при", Кубок чемпионок Газпром Алины Кабаевой "Газпром детям" среди клубов',
-        is_main: false,
-        dt_start: "2024-09-09T10:00:00+03:00",
-        dt_end: "2024-09-15T23:59:00+03:00",
-        dt_create: "2023-03-07T04:19:50+03:00",
-      },
-    ],
-  },
-};
-
 function App() {
-  const { loading, error } = useQuery<VideostandEventResponse>(
+  const { loading, error, data } = useQuery<VideostandEventResponse>(
     gql`
       query videostandEvents($videostand_id: ID!) {
         videostandEvents(videostand_id: $videostand_id) {
@@ -63,12 +31,12 @@ function App() {
     }
   );
 
-  const data = testData;
+  /*   const data = testData; */
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  if (data?.videostandEvents.current_and_upcoming.length === 0) {
+  if (!data || data?.videostandEvents.current_and_upcoming.length === 0) {
     return <Clock />;
   }
 
@@ -111,3 +79,35 @@ function App() {
 }
 
 export default App;
+
+const testData = {
+  videostandEvents: {
+    current_and_upcoming: [
+      {
+        __typename: "VideostandEvent", // 2
+        title: "Чемпионат Москвы",
+        is_main: false,
+        dt_start: "2024-09-02T10:00:00+03:00",
+        dt_end: "2024-09-03T23:59:00+03:00",
+        dt_create: "2024-01-31T02:39:01+03:00",
+      },
+      /* {
+        __typename: "VideostandEvent", // 1
+        title: "Первенство Москвы",
+        is_main: true,
+        dt_start: "2024-08-30T10:00:00+03:00",
+        dt_end: "2024-09-04T21:00:00+03:00",
+        dt_create: "2024-01-24T05:56:29+03:00",
+      }, */
+      {
+        __typename: "VideostandEvent", // 3
+        title:
+          'Международные соревнования "Гран-при", Кубок чемпионок Газпром Алины Кабаевой "Газпром детям" среди клубов',
+        is_main: false,
+        dt_start: "2024-09-09T10:00:00+03:00",
+        dt_end: "2024-09-15T23:59:00+03:00",
+        dt_create: "2023-03-07T04:19:50+03:00",
+      },
+    ],
+  },
+};
