939dfca2393bafdd10a903afc637dba5e2d6fe81
Re:Gelu <morandva@yandex.ru>
2024-09-01 19:49:52 +0300

    Add font and @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import fonts from "unplugin-fonts/vite";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    fonts({
+      google: {
+        families: ["Ubuntu"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
