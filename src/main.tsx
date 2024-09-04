deb7342ae6eb871b8fa5e4bb5c1699179f028d21
Re:Gelu <morandva@yandex.ru>
2024-09-04 16:47:02 +0300

    Fix sorting error

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { apolloClient } from "@/shared/api";
-import { theme } from "@/shared/theme.ts";
+import { theme } from "@/shared/theme";
 import { ApolloProvider } from "@apollo/client";
 import { Center, MantineProvider } from "@mantine/core";
 
